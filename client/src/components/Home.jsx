8fb9506fdbf987ca9dd4ce839d9779e3022cb709
carlos-sola <carlos.sola.z.95@gmail.com>
2022-09-13 20:18:19 -0300

    por iniciar componente home

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css'
+import NavBar from './NavBar/NavBar';
+
+export default function Home(){
+    return (
+        <div>
+            <NavBar/>
+            <p>Este es el home</p>
+        </div>
+    )
+}
