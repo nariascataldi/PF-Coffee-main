b004c35630b6f63c5b7ee49bf2669cec541cd730
RodriZach <rodri_h87@hotmail.com>
2022-09-14 20:53:18 -0300

    creadas las rutas de providers con llamado por id y name

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,28 +1,49 @@
 const { getDiets } = require('../utils/getDiets.js');
 const { getCategories } = require('../utils/getCategories.js');
-const  getProducts  = require('../utils/getProducts.js');
-const  postProduct  = require('../utils/postProduct.js');
-const  postProvider = require('../utils/postProvider.js');
+const getProducts = require('../utils/getProducts.js');
+const postProduct = require('../utils/postProduct.js');
+const postProvider = require('../utils/postProvider.js');
 const { getProductsQy } = require('../utils/getProductsQy.js');
 const getIDproduct = require('../utils/getIDproduct');
 const altProduct = require('../utils/altProduct');
 const deleteProduct = require('../utils/deleteProduct.js');
+const getProviders = require('../utils/getProviders.js');
+const getIdProvider = require('../utils/getIDprovider.js');
 
-const productsGet = async(req, res, next)=>{
+const productsGet = async (req, res, next) => {
   try {
-    let { data } = req.query;  console.log(data);
+    let { data } = req.query; console.log(data);
     if (!data) {
       let prdts = await getProducts() || [];
       console.log(prdts.length);
       return res.send(prdts)  //    petición NO  probada !!!!!! --
     };
     let response = await getProductsQy(data);
-    if (response.length === 0) response = [{msg: 'There are no products with that word in their title. Try another possible denomination'}];
+    if (response.length === 0) response = [{ msg: 'There are no products with that word in their title. Try another possible denomination' }];
     res.send(response)              //    petición NO  probada !!!!!! --
   } catch (e) { next(e) }
 };
 
-const prodIDget = async(req, res, next)=>{
+const providersGet = async (req, res, next) => {
+  try {
+    const { name } = req.query;
+    let providers = await getProviders();
+    if (name) {
+      const nameProvider = providers.filter(a => a.name.toLowerCase().includes(name.toLowerCase()));
+      if (nameProvider.length) {
+        res.send(nameProvider);
+      } else {
+        res.send("No se encontro ningun proveedor")
+      }
+    } else {
+      return res.send(providers)
+    }
+  } catch (error) {
+    next(error)
+  }
+};
+
+const prodIDget = async (req, res, next) => {
   try {
     let { id } = req.params; console.log(id);
     let response = await getIDproduct(id) || {};
@@ -30,55 +51,65 @@ const prodIDget = async(req, res, next)=>{
   } catch (e) { next(e) }
 };
 
-const prodPost = async(req, res, next)=>{
+const providerIDget = async (req, res, next) => {
   try {
-    console.log('input en controllers API: ',req.body);
+    let { id } = req.params;
+    let response = await getIdProvider(id);
+    res.send(response)
+  } catch (error) {
+    next(error)
+  }
+};
+
+const prodPost = async (req, res, next) => {
+  try {
+    console.log('input en controllers API: ', req.body);
     let response = await postProduct(req.body) || {};
     res.send(response)   //    petición NO  probada !!!!!! --
   } catch (e) { next(e) }
 };
 
-const providerPost = async(req, res, next) => {
+const providerPost = async (req, res, next) => {
   try {
     let response = await postProvider(req.body);
     res.send(response)
-  } catch (error) { 
-      next(error)
+  } catch (error) {
+    next(error)
   }
 };
 
-const dietsGet = async(req, res, next)=>{
+const dietsGet = async (req, res, next) => {
   try {
     let o = await getDiets() || [];
     res.send(o)            //   petición NO  probada !!!!!! --
   } catch (e) { next(e) }
 };
 
-const categoriesGet = async(req, res, next)=>{
+const categoriesGet = async (req, res, next) => {
   try {
     let o = await getCategories() || [];
     res.send(o)            //     petición NO  probada !!!!!! --
   } catch (e) { next(e) }
 };
 
-const altAttribute = async (req, res, next)=>{
-	try {
-		let { attribute } = req.params;
-	  let { id, value } = req.query;
-    
-    console.log(id);  console.log(attribute); console.log(value);
-    
-		let myAlt = await altProduct(id, attribute, value) || {};
-		res.send(myAlt)            // petición NO  probada !!!!!! --
-	} catch (e) { next (e) }
+const altAttribute = async (req, res, next) => {
+  try {
+    let { attribute } = req.params;
+    let { id, value } = req.query;
+
+    console.log(id); console.log(attribute); console.log(value);
+
+    let myAlt = await altProduct(id, attribute, value) || {};
+    res.send(myAlt)            // petición NO  probada !!!!!! --
+  } catch (e) { next(e) }
 };
 
-const prodIDremove = async (req, res, next)=>{
-	try {
-	  let { id } = req.query;
-		let response = await deleteProduct(id) || {};
-		res.send(response)            // petición NO  probada !!!!!! --
-	} catch (e) { next (e) }
+const prodIDremove = async (req, res, next) => {
+  try {
+    let { id } = req.query;
+    let response = await deleteProduct(id) || {};
+    res.send(response)            // petición NO  probada !!!!!! --
+  } catch (e) { next(e) }
 };
 
 module.exports = {
@@ -89,5 +120,7 @@ module.exports = {
   categoriesGet,
   altAttribute,
   prodIDremove,
-  providerPost
+  providerPost,
+  providersGet,
+  providerIDget
 }
