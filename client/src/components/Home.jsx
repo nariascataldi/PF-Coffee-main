1ac454aa8f8ea83517bb829e352f53ad673b42b5
carlos-sola <carlos.sola.z.95@gmail.com>
2022-09-13 22:30:03 -0300

    empezando Home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { getAllProducts } from '../redux/actions';
 import './Home.css'
 import NavBar from './NavBar/NavBar';
 
 export default function Home(){
+    const dispatch = useDispatch
+    useEffect(()=> {
+        dispatch(getAllProducts());
+},[])
     return (
         <div>
             <NavBar/>
