f4e5639fb40386686b5a72543c265b33d3d9ad40
Gaspar Perez <100445192+gasparperez2@users.noreply.github.com>
2022-09-14 18:58:11 -0300

    Footer (#44)
    * m
    
    * Footer
    
    * traer productos

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getAllProducts } from '../../redux/actions/index.js';
 import Footer from '../Footer/Footer.jsx';
 import NavBar from '../NavBar/NavBar.jsx';
 import './Home.css'
 
 export default function Home(){
+
     const dispatch = useDispatch();
+
+    let allProducts = useSelector(state => state.allProducts)
 
     React.useEffect(()=> {
         dispatch(getAllProducts());
@@ -14,6 +17,7 @@ export default function Home(){
 
     return (
         <div>
+            {console.log(allProducts)}
             <NavBar />
             <p>Este es el home</p>
             <Footer />
