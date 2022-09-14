74e32756a3fed964c75c2a16e91f133ed20b532f
Gaspar Perez <100445192+gasparperez2@users.noreply.github.com>
2022-09-14 18:24:36 -0300

    Nav (#42)
    * m
    
    * cambios en el footer y Nav

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { getAllProducts } from '../redux/actions';
+import { getAllProducts } from '../../redux/actions/index.js';
+import Footer from '../Footer/Footer.jsx';
+import NavBar from '../NavBar/NavBar.jsx';
 import './Home.css'
-import NavBar from './NavBar/NavBar';
 
 export default function Home(){
-    const dispatch = useDispatch
-    useEffect(()=> {
+    const dispatch = useDispatch();
+
+    React.useEffect(()=> {
         dispatch(getAllProducts());
     },[])
 
     return (
         <div>
+            <NavBar />
             <p>Este es el home</p>
+            <Footer />
         </div>
     )
 }
