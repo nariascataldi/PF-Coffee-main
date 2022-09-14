d10a8514c82155bcab9fec937f82ac668a804d77
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-09-14 10:36:50 -0300

    prueba

--- api/src/utils/pushDatabase/pushProducts.js
+++ api/src/utils/pushDatabase/pushProducts.js
@@ -5,7 +5,7 @@ let json = Object(product)
 
 console.log(json);
 
-async function pushProducts (json) {
+async function pushProducts(json) {
 
   try {
     for (let prop in json) {
@@ -13,27 +13,27 @@ async function pushProducts (json) {
       let category = prop;
 
       json[prop].forEach(obj => {
-        Product.create(obj)       
-          .then(async(myProduct) => {
-              // let allDts = await Diet.findAll();
-              let allCategories = await Category.findAll();
-              // await Promise.all( [allDts, allCategories] ); // si fuesen más promesas
+        Product.create(obj)
+          .then(async (myProduct) => {
+            // let allDts = await Diet.findAll();
+            let allCategories = await Category.findAll();
+            // await Promise.all( [allDts, allCategories] ); // si fuesen más promesas
 
-              // let newsDts = allDts.filter(o=> obj.diets.includes(o.name));
-              let myCateg = allCategories.find(o=> o.name === category);
-              
-              // let incDiets = newsDts.map( (d)=> myProduct.addDiet(d.id) );
-              let incCat = myProduct.addCategory(myCateg.id);
-              
+            // let newsDts = allDts.filter(o=> obj.diets.includes(o.name));
+            let myCateg = allCategories.find(o => o.name === category);
+
+            // let incDiets = newsDts.map( (d)=> myProduct.addDiet(d.id) );
+            let incCat = myProduct.addCategory(myCateg.id);
+
           })
-          .catch(e=> console.log('falló en Product.create():', e));
+          .catch(e => console.log('falló en Product.create():', e));
       });
-      
+
     }
   } catch (error) {
     console.log('error al forkear el db.json:', error);
   }
-  
+
 };
 
-module.exports  = pushProducts;
+module.exports = pushProducts;
