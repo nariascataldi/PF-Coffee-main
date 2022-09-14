d811b6f6af24d5bd5d3a9abf7726f35736d4f083
gasparperez2 <gasparperez171@gmail.com>
2022-09-14 18:22:51 -0300

    cambios en el footer y Nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import LandingPage from './components/LandingPage/LandingPage';
 import About from './components/About/About';
 import UserCreate from './components/UserCreate/UserCreate';
-
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path='/' element={<LandingPage />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/form' element={<UserCreate />} />
-
-      </Routes>
-    </BrowserRouter>
+      <div>
+        <Routes>
+          <Route exact path='/' element={<LandingPage />} />
+          <Route path='/home' element={< Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/form' element={<UserCreate />} />
+        </Routes>
+      </div>
   );
 }
 
