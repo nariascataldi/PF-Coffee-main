29bf249635fae468d140f67b1013f7284d8b6f35
Jonathandanielperez <jonathantj63@gmail.com>
2022-09-14 11:32:31 -0300

    agregando el paginado al componenete cards

---
+++ client/src/components/Cards/Cards.jsx
@@ -0,0 +1,50 @@
+import React from "react";  
+import {useState} from 'react'
+import { useSelector } from "react-redux";
+import Card from "../Card";
+import Paginated from "../../Paginated";
+
+export default function Cards (){
+    const {allProducts} = useSelector(state=>state)
+
+        //paginado
+        const[currentPage, setCurrentPage]=useState(1)
+        const[productsPerPage, setProductsPerPage]= useState(9)
+        const indexOfLast= currentPage * productsPerPage
+        const indexOfFirst= indexOfLast - productsPerPage
+        const currentProducts= allProducts.slice(indexOfFirst, indexOfLast)
+        const paginated= (pageNumber)=>{
+            setCurrentPage(pageNumber)
+
+    if(!currentProducts.length){
+        return (
+            <div><h3>Not found!</h3></div>
+        )
+    }
+    return (
+        <div>
+
+            <div>
+                <Paginated 
+                    productsPerPage={productsPerPage}
+                    allProducts={allProducts.length}
+                    paginated={paginated}
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                />
+            </div>
+
+            <div>
+                {currentProducts && currentProducts.map(p=>{
+                    return <Card
+                    name={p.name}
+                    image={p.image}
+                    title={p.title}
+                    price={p.price}
+                    />
+            })}  
+            </div>
+
+        </div>
+    ) 
+}
