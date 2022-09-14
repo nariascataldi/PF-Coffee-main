b71686bbf2646ca62588bec7232b046ed43c9ada
gasparperez2 <gasparperez171@gmail.com>
2022-09-14 19:11:42 -0300

    m

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-=======
 import { Route, Routes } from "react-router-dom";
->>>>>>> 74e32756a3fed964c75c2a16e91f133ed20b532f
 import LandingPage from './components/LandingPage/LandingPage';
 import About from './components/About/About';
 import UserCreate from './components/UserCreate/UserCreate';
@@ -12,16 +8,6 @@ import Home from './components/Home/Home';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <BrowserRouter>
-      <Routes>
-        <Route exact path='/' element={<LandingPage />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/form' element={<UserCreate />} />
-
-      </Routes>
-    </BrowserRouter>
-=======
       <div>
         <Routes>
           <Route exact path='/' element={<LandingPage />} />
@@ -30,7 +16,6 @@ function App() {
           <Route path='/form' element={<UserCreate />} />
         </Routes>
       </div>
->>>>>>> 74e32756a3fed964c75c2a16e91f133ed20b532f
   );
 }
 
