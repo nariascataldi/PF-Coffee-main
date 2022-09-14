2074728e11f03fdea07ca6bbcbb4674ea1d39809
Matias <matiasdentoni@gmail.com>
2022-09-14 20:59:15 -0300

    mitad de pruebas

--- api/src/utils/postProduct.js
+++ api/src/utils/postProduct.js
@@ -1,37 +1,47 @@
-const { Product, Diet, Category } = require('../db.js');
+const { Product, Diet, Category, Provider } = require('../db.js');
 
 let postProduct = async(obj)=>{
-  console.log('input en utils postProduct API: ', obj);
-  let { title, image, price, description, likes, diets, categories  } = obj;
-  // capitalize
-  if (title.includes(' ')) {
-    title = title.split(' ').map(p=> p.slice(0,1).toUpperCase().concat(p.slice(1).toLowerCase())).join(' ')
-  } else { title = title[0].toUpperCase().concat(title.slice(1).toLowerCase()) };
+  try {
+    console.log('input en utils postProduct API: ', obj);
+    let { id, title, price, description, image, disable, like, stock, sale_count, diets, categories, providers  } = obj;
+    // capitalize
+    if (title.includes(' ')) {
+      title = title.split(' ').map(p=> p.slice(0,1).toUpperCase().concat(p.slice(1).toLowerCase())).join(' ')
+    } else { title = title[0].toUpperCase().concat(title.slice(1).toLowerCase()) };
 
 
-  let prod = await Product.create({ title, image, price, description, likes });  
+    let prod = await Product.create({ id, title, price, description, image, disable, like, stock, sale_count });  
 
-  let prodCread = await Product.findOne({ where: { title: title } }); //  console.log(prodCread); console.log(title);
+    let prodCread = await Product.findOne({ where: { title: title } }); //  console.log(prodCread); console.log(title);
 
-  if (diets && diets.length > 0 ){
-    Diet.findAll() 
-      .then(r=> r.filter(o=> diets.includes(o.name)) )
-      .then(r=> r.map( async(d)=> await prodCread.addDiet(d.id) ) )   // d.dataValues.id
-      .catch(e=> console.log(e))   
-  };  
-  if (categories && categories.length > 0 ){
-    Category.findAll()
-      .then(r=> r.filter(o=> categories.includes(o.name)) )
-      .then(r=> r.map( async(dt)=> await prodCread.addCategory(dt.id) ) )   // dt.dataValues.id
-      .catch(e=> console.log(e))
-  };  
+    if (diets && diets.length > 0 ){
+      Diet.findAll() 
+        .then(r=> r.filter(o=> diets.includes(o.name)) )
+        .then(r=> r.map( async(d)=> await prodCread.addDiet(d.id) ) )   // d.dataValues.id
+        .catch(e=> console.log(e))   
+    };  
+    if (categories && categories.length > 0 ){
+      Category.findAll()
+        .then(r=> r.filter(o=> categories.includes(o.name)) )
+        .then(r=> r.map( async(c)=> await prodCread.addCategory(c.id) ) )   // dt.dataValues.id
+        .catch(e=> console.log(e))
+    };  
+    if (providers && providers.length > 0 ){
+      Provider.findAll()
+        .then(r=> r.filter(o=> providers.includes(o.name)) )
+        .then(r=> r.map( async(p)=> await prodCread.addProvider(p.id) ) )   // dt.dataValues.id
+        .catch(e=> console.log(e))
+    };
+      
+    let prodCreated = await Product.findOne({
+      where: { id: prodCread.id },
+      include: [ Category ] // Diet,
+    });
+
+    return prodCreated;
     
-  let prodCreated = await Product.findOne({
-    where: { id: prodCread.id },
-    include: [ Diet, Category ]
-  });
-
-  return prodCreated;
+  } catch (error) { console.log(error) };
+  
 };
 
 module.exports = postProduct;
