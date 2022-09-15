7da3ca27bb6e05c1af8f7ba8c3bb68ebfa7f34e6
RodriZach <rodri_h87@hotmail.com>
2022-09-15 16:45:12 -0300

    back actualizado y funcionando

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -19,11 +19,11 @@ const productsGet = async (req, res, next) => {
     if (!data) {
       let prdts = await getProducts() || [];
       console.log(prdts.length);
-      return res.send(prdts)  //    petición NO  probada !!!!!! --
+      return res.send(prdts)  //    petición   probada !!!!!! --
     };
     let response = await getProductsQy(data);
     if (response.length === 0) response = [{ msg: 'There are no products with that word in their title. Try another possible denomination' }];
-    res.send(response)              //    petición NO  probada !!!!!! --
+    res.send(response)              //    petición   probada !!!!!! --
   } catch (e) { next(e) }
 };
 
@@ -50,7 +50,7 @@ const prodIDget = async (req, res, next) => {
   try {
     let { id } = req.params; console.log(id);
     let response = await getIDproduct(id) || {};
-    res.send(response)              //    petición NO  probada !!!!!! --
+    res.send(response)              //    petición   probada !!!!!! --
   } catch (e) { next(e) }
 };
 
@@ -68,30 +68,21 @@ const prodPost = async (req, res, next) => {
   try {
     console.log('input en controllers API: ', req.body);
     let response = await postProduct(req.body) || {};
-    res.send(response)   //    petición NO  probada !!!!!! --
+    res.send(response)   //    petición   probada !!!!!! --
   } catch (e) { next(e) }
-};
-
-const providerPost = async (req, res, next) => {
-  try {
-    let response = await postProvider(req.body);
-    res.send(response)
-  } catch (error) {
-    next(error)
-  }
 };
 
 const dietsGet = async (req, res, next) => {
   try {
     let o = await getDiets() || [];
-    res.send(o)            //   petición NO  probada !!!!!! --
+    res.send(o)            //   petición   probada !!!!!! --
   } catch (e) { next(e) }
 };
 
 const categoriesGet = async (req, res, next) => {
   try {
     let o = await getCategories() || [];
-    res.send(o)            //     petición NO  probada !!!!!! --
+    res.send(o)            //     petición   probada !!!!!! --
   } catch (e) { next(e) }
 };
 
@@ -112,7 +103,7 @@ const altAttribute = async (req, res, next)=>{
     console.log(id);  console.log(attribute); console.log(value);
     
 		let myAlt = await altProduct(id, attribute, value) || {};
-		res.send(myAlt)            // petición NO  probada !!!!!! --
+		res.send(myAlt)            // petición   probada !!!!!! --
 	} catch (e) { next (e) }
 };
 
@@ -120,7 +111,7 @@ const prodIDremove = async (req, res, next) => {
   try {
     let { id } = req.query;
     let response = await deleteProduct(id) || {};
-    res.send(response)            // petición NO  probada !!!!!! --
+    res.send(response)            // petición   probada !!!!!! --
   } catch (e) { next(e) }
 };
 
