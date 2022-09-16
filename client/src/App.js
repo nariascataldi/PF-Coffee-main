f2c82bff37a11121a875bc5540b9ef62087c653b
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-09-16 16:05:23 -0300

    body admin

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import LandingPage from './components/LandingPage/LandingPage';
 import About from './components/About/About';
 import Home from './components/Home/Home';
 import Perfil from './components/Account/Perfil';
+import HomeAdmin from './components/Admin/HomeAdmin/HomeAdmin';
 
 import './App.css';
 
@@ -15,6 +16,8 @@ function App() {
           <Route path='/home' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/form' element={<Perfil />} />
+          <Route exact path='/homeAdmin' element={<HomeAdmin />} />
+
         </Routes>
       </div>
   );
