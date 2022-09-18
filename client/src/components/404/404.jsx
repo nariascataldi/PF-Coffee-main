a0e9186ceee43d473e2512e76d93a26ef7a640be
gasparperez2 <gasparperez171@gmail.com>
2022-09-18 20:45:17 -0300

    404 terminado

--- client/src/components/404/404.jsx
+++ client/src/components/404/404.jsx
@@ -16,7 +16,11 @@ const NotFound = () => {
                 <img src={imgNotFound} alt="notFound" className='img-responsive center-block d-block mx-auto' id='image'/>
                 <div className='imgText'>
                     <h5 className='titleNotFound'>Parece que esta página no existe</h5>
-                    <Link to='/home'>Home</Link>
+                    <Link to='/home' className='homeButton'>
+                        <span>
+                            Ir a la página principal
+                        </span>
+                    </Link>
                 </div>
             </div>
             <div>
