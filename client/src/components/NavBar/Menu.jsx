3b4b691a6d463a5e1287e41e75074c5525c89afe
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-09-21 13:59:39 -0300

    css menu y navbar retorcedi, me ganó

--- client/src/components/NavBar/Menu.jsx
+++ client/src/components/NavBar/Menu.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { useDispatch, useSelector } from "react-redux";
 import { Link } from 'react-router-dom';
-import style from './Menu.module.css';
+import './Menu.css';
 
 export default function Menu({menu}){
 
@@ -15,18 +15,18 @@ export default function Menu({menu}){
     //  console.log(seasonactivities);
     //  console.log(seasonSinDuplicado)
     return  (
-        <div className={style.menu_container ${menu ? "open" : ""}}> 
-        <div className={style.box_menu1}>
+        <div className={`menu-container ${menu ? "open" : ""}`}> 
+        <div className='box-menu1'>
         <p></p>
         </div>
-        <div className={style.box_menu}>
-            <Link to='/productAdmin'><button className={style.menubutton_crate_product}>Add a new product</button></Link>
+        <div className='box-menu'>
+            <Link to='/productAdmin'><button className='menubutton-crate-product'>Add a new product</button></Link>
         </div>
-        <div className={style.box_menu}>
-        <button className={style.menubutton_crate_product}>Add new Provider</button>
+        <div className='box-menu'>
+        <button className='menubutton-crate-product'>Add new Provider</button>
         </div>
-        <div className={style.box_menu}>
-        <button className={style.menubutton_crate_product}>Log in</button>
+        <div className='box-menu'>
+        <button className='menubutton-crate-product'>Log in</button>
         </div>
     </div>)
 
