c543af43bd4e6af778c4e0e7753f4a7a620b3791
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-09-22 20:41:48 -0300

    data json

--- client/src/components/Admin/HomeAdmin/HomeAdmin.jsx
+++ client/src/components/Admin/HomeAdmin/HomeAdmin.jsx
@@ -35,6 +35,7 @@ export default function HomeAdmin() {
     dispatch(setFilterState({ title: busqueda }));
     setBusqueda('')
   }
+
   return (
     <div className='home-container'>
       <NavBarAdmin></NavBarAdmin>
