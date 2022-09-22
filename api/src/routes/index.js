cf9a7df0e56c7b4aecef630811142bd0f6cf942b
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-09-22 09:23:27 -0300

    back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,25 +1,25 @@
 const { Router } = require('express');
-const middlewareAuth = require('../middlewareAuth');
-const middlewareAdmin = require('../middlewareAdmin');
+// const middlewareAuth = require('../middlewareAuth');
+// const middlewareAdmin = require('../middlewareAdmin');
 
 
 const { productsGet,
-        prodIDget,
-        dietsGet,
-        categoriesGet,
-        commentGet,
-        prodIDremove,
-        prodPost,
-        providerPost,
-        providersGet,
-        providerIDget,
-        altAttribute,
-        commentPost,        
-        usersGet,
-        userIDget  } = require('../controllers');
+  prodIDget,
+  dietsGet,
+  categoriesGet,
+  commentGet,
+  prodIDremove,
+  prodPost,
+  providerPost,
+  providersGet,
+  providerIDget,
+  altAttribute,
+  commentPost,
+  usersGet,
+  userIDget } = require('../controllers');
 
 const { userRegist,
-        userLogin } = require('../controllers/authControllers.js');
+  userLogin } = require('../controllers/authControllers.js');
 
 // import * as ctrls from '../controllers ---> ej: ctrls.productGet   (babel)
 
@@ -40,7 +40,7 @@ router.get('/comment', commentGet)   // ruta probada !!!!!! --
 
 router.get('/providers', providersGet);    // ruta probada !!!!!! --
 
-router.get('/providers/:id', middlewareAdmin, providerIDget);      // ruta probada !!!!!! --
+router.get('/providers/:id', providerIDget);      // ruta probada !!!!!! -- middlewareAdmin,
 
 router.get('/users', usersGet);    // ruta NO probada !!!!!! --
 
@@ -56,15 +56,15 @@ router.delete('/products/remove', prodIDremove);  // ruta probada !!!!!! --
 
 //---------------POST
 
-router.post('/products', middlewareAdmin, prodPost);    // ruta probada !!!!!! --
+router.post('/products', prodPost);    // ruta probada !!!!!! -- middlewareAdmin,
 
-router.post("/providers", middlewareAdmin, providerPost);   // ruta probada !!!!!! --
+router.post("/providers", providerPost);   // ruta probada !!!!!! -- middlewareAdmin,
 
-router.post('/comment', middlewareAuth, commentPost);     // ruta probada !!!!!! --
+router.post('/comment', commentPost);     // ruta probada !!!!!! -- middlewareAuth,
 
 // router.post("/orders", middlewareAuth, orderPost);   // ruta NO probada !!!!!! --
 
-                     //----Validation
+//----Validation
 
 router.post('/users/registration', userRegist);     // ruta NO probada !!!!!! --
 
@@ -73,7 +73,7 @@ router.post('/users/login', userLogin);     // ruta NO probada !!!!!! --
 
 //---------------PUT
 
-router.put('/products/:attribute', middlewareAdmin, altAttribute);  // ruta probada !!!!!! --
+router.put('/products/:attribute', altAttribute);  // ruta probada !!!!!! -- middlewareAdmin,
 
 
 
