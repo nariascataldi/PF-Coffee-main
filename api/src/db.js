8e56466a02e83b51bb10d41dfeee8059d92edd03
Luciana <97133350+LucianaSoledad86@users.noreply.github.com>
2022-09-26 02:47:46 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -28,7 +28,7 @@ let sequelize =
         },
         ssl: true,
       })
-    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/coffee`, {
+    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
         logging: false, // set to console.log to see the raw SQL queries
         native: false, // lets Sequelize know we can use pg-native for ~30% more speed
       });
