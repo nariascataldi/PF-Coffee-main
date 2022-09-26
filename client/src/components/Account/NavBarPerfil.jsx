58fe6308e3913554751d97c94b6a016f05f57d87
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-09-26 11:04:17 -0300

    para el developer

--- client/src/components/Account/NavBarPerfil.jsx
+++ client/src/components/Account/NavBarPerfil.jsx
@@ -6,10 +6,10 @@ import style from "../../styles/NavBarPerfil.module.css";
 const ShinyButton = () => {
   return (
     <div className={style.container}>
-      <NavLink exact to='../home' className={style.navBarPerfil} >Perfil</NavLink>
-      <NavLink exact to='../home' className={style.navBarPerfil} >Direcciones</NavLink>
-      <NavLink exact to='../home' className={style.navBarPerfil} >Pedidos Anteriores</NavLink>
-      <NavLink exact to='../home' className={style.navBarPerfil} >Salir</NavLink>
+      <NavLink exact to='../' className={style.navBarPerfil} >Perfil</NavLink>
+      <NavLink exact to='../' className={style.navBarPerfil} >Direcciones</NavLink>
+      <NavLink exact to='../' className={style.navBarPerfil} >Pedidos Anteriores</NavLink>
+      <NavLink exact to='../' className={style.navBarPerfil} >Salir</NavLink>
     </div>
   );
 
