4a7a5c9601f695c2b737bb7c895b54619d335112
carlos-sola <carlos.sola.z.95@gmail.com>
2022-09-29 15:37:19 -0300

    trabajo en checkout payed

--- client/src/components/Checkout/CheckoutConfirm.jsx
+++ client/src/components/Checkout/CheckoutConfirm.jsx
@@ -5,6 +5,8 @@ import { cartEmptying } from "../../redux/actions";
 import Footer from "../Footer";
 import NavBar from "../NavBar";
 import { BsFillPatchCheckFill} from "react-icons/bs";
+import { reduceCart } from "../../utils/reduceCart";
+import logo from '../../assets/logo_coffee.png'
 import '../Checkout/Checkout.css'
 
 
@@ -26,16 +28,28 @@ export default function CheckoutConfirm (){
 
   console.log('order ',order)
   console.log('id order ',idOrder)
+  //Detalle de la compra
+  const reducedCart =reduceCart(fillCart)
+  console.log('reduce.cart ',reducedCart)
+  let sumaTotal = 0
+    for(let i = 0; i < fillCart.length;i++){
+        sumaTotal = sumaTotal + fillCart[i].price
+    }
 
   useEffect (()=>{
       if(statusPago === 'approved'){
         console.log('status pago = ',statusPago)
-        return dispatch(cartEmptying()) ; 
+          setTimeout(()=>{
+            return dispatch(cartEmptying()) 
+        },15000); 
       }
   },[]);
   useEffect(() => {
     localStorage.setItem("carrito", JSON.stringify(fillCart));
   }, [fillCart]);
+  
+
+    
 
     return (
         <div className='fill-cart-wraper'>
@@ -45,6 +59,20 @@ export default function CheckoutConfirm (){
             <div >
                 <div className='cart-container'>
                     <h1> Payment status:  " {statusPago} "  <BsFillPatchCheckFill className='icon-pay-approved'/></h1>
+                    <div className='detail-cart-shop-approved'>
+                        <ul className='list-group list-group-flush'>
+                            <li className='list-group-item fondo'><h2>TICKET</h2></li>
+                            <li className='list-group-item fondo'><h3>Products :</h3>
+                                {reducedCart.length && reducedCart.map((e) => {
+                                 return (
+                                    <li className='list-group-item fondo'><p>  -{e.title} : $ {e.price}  {`( x ${e.quantity} u.)`}</p>
+                                    </li>  
+                                    )
+                                })}
+                            </li>
+                            <li className='list-group-item fondo'><h2>Paid : ${sumaTotal}</h2> </li>
+                        </ul>
+                    </div>
                     <Link to='/'>
                     <button className='btn-statuspay-tohome'>Back to home</button>
                     </Link>
