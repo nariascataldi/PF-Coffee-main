4f84b8ea91571bf0ffdb1f94ff0c784324b72838
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-10-02 21:24:42 -0300

    tab

--- client/src/components/Admin/HomeAdmin/HomeAdmin.jsx
+++ client/src/components/Admin/HomeAdmin/HomeAdmin.jsx
@@ -1,4 +1,4 @@
-import React , {useState} from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { getAllProducts, postNewsletter } from '../../../redux/actions/index.js';
 
@@ -15,12 +15,13 @@ import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
-import ListProducts from '../Product/CRUD Product/ListProduct';
-import ListProviders from '../../Provider/CRUD Provider/ListProvider.jsx';
+
 import FormProvider from '../../Provider/CRUD Provider/PrividerCreate';
 import Newsletter from '../Newsletter/Newsletter.jsx';
 import OrdersHistory from '../Orders/OrdersHistory.jsx'
-// import Providers from '../../Provider/Providers.jsx';
+import Product from '../Product/Product.jsx';
+import Provider from '../../Provider/ProvHome.jsx';
+
 
 export default function HomeAdmin() {
   //---SearchBar---
@@ -32,34 +33,23 @@ export default function HomeAdmin() {
 
   return (
     <div className={style.home_container}>
-      <NavBarAdmin/>
+      <NavBarAdmin />
       <Tab.Container id="left-tabs-example" defaultActiveKey="homeAdmin">
         <Row>
           <Col sm={2}>
             <Nav variant="pills" className="flex-column">
-              <Nav.Item>
-                <Nav.Link eventKey="homeAdmin">Home Admin</Nav.Link>
-              </Nav.Item>
-              <Nav.Item> <Nav.Link eventKey="prodCreate">Create Product</Nav.Link> </Nav.Item>
-              <Nav.Item> <Nav.Link eventKey="prodEdit">Edit Product</Nav.Link> </Nav.Item>
-              {/* <Nav.Item> <Nav.Link eventKey="provList">Provider</Nav.Link> </Nav.Item> */}
-              <Nav.Item> <Nav.Link eventKey="addProvider">Crear Provider</Nav.Link> </Nav.Item>
-              <Nav.Item> <Nav.Link eventKey="provEdit">Edit Provider</Nav.Link> </Nav.Item>
+              <Nav.Item> <Nav.Link eventKey="homeAdmin">Home Admin</Nav.Link> </Nav.Item>
+              <Nav.Item> <Nav.Link eventKey="prod">Product</Nav.Link> </Nav.Item>
+              <Nav.Item> <Nav.Link eventKey="prov">Provider</Nav.Link> </Nav.Item>
               <Nav.Item> <Nav.Link eventKey="mailNews">Send Newsletter</Nav.Link> </Nav.Item>
               <Nav.Item> <Nav.Link eventKey="orders">Order's History</Nav.Link> </Nav.Item>
             </Nav>
           </Col>
           <Col sm={10}>
             <Tab.Content>
-              <Tab.Pane eventKey="homeAdmin">
-              <SearchBar />
-                <Cards />
-              </Tab.Pane>
-              <Tab.Pane eventKey="prodCreate"> <ProductAdmin /> </Tab.Pane>
-              <Tab.Pane eventKey="prodEdit"> <ListProducts />  </Tab.Pane>
-              {/* <Tab.Pane eventKey="provList"> <Providers />  </Tab.Pane> */}
-              <Tab.Pane eventKey="addProvider"> <FormProvider />  </Tab.Pane>
-              <Tab.Pane eventKey="provEdit"> <ListProviders />  </Tab.Pane>
+              <Tab.Pane eventKey="homeAdmin"> <SearchBar /> <Cards /> </Tab.Pane>
+              <Tab.Pane eventKey="prod"> <Product />  </Tab.Pane>
+              <Tab.Pane eventKey="prov"> <Provider />  </Tab.Pane>
               <Tab.Pane eventKey="mailNews"> <Newsletter />  </Tab.Pane>
               <Tab.Pane eventKey="orders"> <OrdersHistory />  </Tab.Pane>
             </Tab.Content>
