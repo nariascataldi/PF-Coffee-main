98d78f1df7369b60ae9d808be3d84bcad9338fca
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-10-03 00:41:17 -0300

    colores

--- client/src/components/Admin/HomeAdmin/HomeAdmin.jsx
+++ client/src/components/Admin/HomeAdmin/HomeAdmin.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { getAllProducts, postNewsletter } from '../../../redux/actions/index.js';
+import logo from '../../../assets/logo_coffee.png'
 
 import Cards from '../CardsAdmin/CardsAdmin.jsx';
 import ProductAdmin from '../Product/ProductAdmin.jsx';
@@ -15,6 +16,7 @@ import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
+import Figure from 'react-bootstrap/Figure'
 
 
 import Newsletter from '../Newsletter/Newsletter.jsx';
@@ -34,29 +36,35 @@ export default function HomeAdmin() {
   return (
     <div className={style.home_container}>
       <NavBarAdmin />
-      <Tab.Container id="left-tabs-example" defaultActiveKey="homeAdmin">
-        <Row>
-          <Col sm={2}>
-            <Nav variant="pills" className="flex-column">
-              <Nav.Item> <Nav.Link eventKey="homeAdmin">Home Admin</Nav.Link> </Nav.Item>
-              <Nav.Item> <Nav.Link eventKey="prod">Product</Nav.Link> </Nav.Item>
-              <Nav.Item> <Nav.Link eventKey="prov">Provider</Nav.Link> </Nav.Item>
-              <Nav.Item> <Nav.Link eventKey="mailNews">Send Newsletter</Nav.Link> </Nav.Item>
-              <Nav.Item> <Nav.Link eventKey="orders">Order's History</Nav.Link> </Nav.Item>
-            </Nav>
-          </Col>
-          <Col sm={10}>
-            <Tab.Content>
-              <Tab.Pane eventKey="homeAdmin"> <SearchBar /> <Cards /> </Tab.Pane>
-              <Tab.Pane eventKey="prod"> <Product />  </Tab.Pane>
-              <Tab.Pane eventKey="prov"> <Provider />  </Tab.Pane>
-              <Tab.Pane eventKey="mailNews"> <Newsletter />  </Tab.Pane>
-              <Tab.Pane eventKey="orders"> <OrdersHistory />  </Tab.Pane>
-            </Tab.Content>
-          </Col>
-        </Row>
-      </Tab.Container>
-
+      <div className={style.figpos}>
+        <Figure>
+          <Figure.Image className={style.figulog} width={17 * 5} height={18 * 5} alt="El Logo" src={logo} />
+        </Figure>
+      </div>
+      <div className={style.conteinTabla}>
+        <Tab.Container id="left-tabs-example" defaultActiveKey="homeAdmin">
+          <Row className={style.rowcon}>
+            <Col sm={2} className={style.colum2}>
+              <Nav variant="pills" className="flex-column">
+                <Nav.Item className={style.bonton} > <Nav.Link eventKey="homeAdmin">Home Admin</Nav.Link> </Nav.Item>
+                <Nav.Item className={style.bonton}> <Nav.Link eventKey="prod">Product</Nav.Link> </Nav.Item>
+                <Nav.Item className={style.bonton}> <Nav.Link eventKey="prov">Provider</Nav.Link> </Nav.Item>
+                <Nav.Item className={style.bonton}> <Nav.Link eventKey="mailNews">Send Newsletter</Nav.Link> </Nav.Item>
+                <Nav.Item className={style.bonton}> <Nav.Link eventKey="orders">Order's History</Nav.Link> </Nav.Item>
+              </Nav>
+            </Col>
+            <Col sm={10} className={style.colum10}>
+              <Tab.Content>
+                <Tab.Pane eventKey="homeAdmin"> <SearchBar /> <Cards /> </Tab.Pane>
+                <Tab.Pane eventKey="prod"> <Product />  </Tab.Pane>
+                <Tab.Pane eventKey="prov"> <Provider />  </Tab.Pane>
+                <Tab.Pane eventKey="mailNews"> <Newsletter />  </Tab.Pane>
+                <Tab.Pane eventKey="orders"> <OrdersHistory />  </Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+      </div>
     </div>
   )
 }
